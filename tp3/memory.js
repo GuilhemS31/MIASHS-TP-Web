4d72b78375b31441cb5c7c9592366858772a576c
GuilhemS31 <guilhem31.susa@hotmail.com>
2018-02-16 15:13:52 +0100

    debut nouv seance

--- tp3/memory.js
+++ tp3/memory.js
@@ -3,8 +3,9 @@
 angular.module('memory',[])
   .factory('LolChamp', ['$resource',
     function($resource) {
-      return $resource('https://global.api.pvp.net/api/lol/static-data/euw/v1.2/champion?locale=fr_FR&api_key=RGAPI-bb62d7e4-9f1b-4764-b829-a63e5f7ff2ca', {}, {
-        query: {
+      return $resource('./champions-info-image-stats-sort.json', {}, {
+      /*return $resource('https://global.api.pvp.net/api/lol/static-data/euw/v1.2/champion?locale=fr_FR&api_key=RGAPI-bb62d7e4-9f1b-4764-b829-a63e5f7ff2ca', {}, {
+        */query: {
           method: 'GET',
           isArray: false
         }
@@ -15,12 +16,14 @@ angular.module('memory',[])
 angular.module("memory")
   .controller('memoryController', ["$scope", "LolChamp", function caseChampCtrl($scope, LolChamp) {
 		
-        var champData = LolChamp.query();
+        //var champData = LolChamp.query();
+		
+        var champData = JSON.parse(ajaxFunction('./champions-info-image-stats-sort.json'));
 		var nbChamp = champData["data"].length();
 		var champSelect = Math.trunc( Math.random() * nbChamp);
 		$scope.champKey = champSelect["data"][champSelect]["name"];
 		/*
-		this.champSelectNbSkin = this.champData["data"][this.champSelect]["skins"].length();
+		var champSelectNbSkin = this.champData["data"][this.champSelect]["skins"].length();
 		random sur nbSkins
 		*/
 		console.log($scope.champKey);
