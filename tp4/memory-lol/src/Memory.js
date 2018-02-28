cce3e2c87f7a924df260659bc9c69ed8a365c6f3
Susa Guilhem <guilhem.susa@gfi.fr>
2018-02-28 17:17:02 +0100

    adaptation class css pour balise react

--- tp4/memory-lol/src/Memory.js
+++ tp4/memory-lol/src/Memory.js
@@ -1,60 +1,73 @@
 import React, { Component } from 'react';
 
 class Memory extends Component {
-	
+	/*
 	constructor(props){
 		super(props);
 		var state = {champs: []} ;
 	}
+	*/
+	state = {champs: [], champsSelect :[]} ;
 	
 	getJson() {
-		fetch('/champions-info-image-stats-sort.json')
+		fetch('/champions-info-image-stats-sort')
 		.then(res => res.json())
         .then(champs => this.setState({ champs }));
 	}
-  
+	
+	selectChamps(){
+		//select 1
+		//put _0 _1 in champsSelect
+		//select 2
+		//check != 1
+		//put _0 _1 in champsSelect
+		//check != 1,2
+		//put _0 _1 in champsSelect
+		//shuffle
+	}
+	
     render() {
-		console.log(state);
+		{console.log(this.state.champs)}
 		return (
 		  <div className="Memory">
-				<div class="row">
-					<div class="col-lg-4 col-md-4 col-xs-6 thumb">
-						<a class="thumbnail">
-							<img id= "Ahri_0" class="img-responsive" src="https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Ahri_0.jpg" alt="Ahri_0"></img>
-							<div class="champName">Ahri</div> 
+				<div className="row">
+					<div className="col-lg-4 col-md-4 col-xs-6 thumb">
+						<a className="thumbnail">
+							<img id= "Ahri_0" className="img-responsive" src="https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Ahri_0.jpg" alt="Ahri_0"></img>
+							<div className="champName">Ahri</div> 
 						</a>
 					</div>
-					<div class="col-lg-4 col-md-4 col-xs-6 thumb">
-						<a class="thumbnail">
-							<img id= "Ahri_0" class="img-responsive" src="https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Ahri_0.jpg" alt="Ahri_0"></img>
-							<div class="champName">Ahri</div> 
+					<div className="col-lg-4 col-md-4 col-xs-6 thumb">
+						<a className="thumbnail">
+							<img id= "Ahri_0" className="img-responsive" src="https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Ahri_0.jpg" alt="Ahri_0"></img>
+							<div className="champName">Ahri</div> 
 						</a>
 					</div>
-					<div class="col-lg-4 col-md-4 col-xs-6 thumb">
-						<a class="thumbnail">
-							<img id= "Ahri_0" class="img-responsive" src="https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Ahri_0.jpg" alt="Ahri_0"></img>
-							<div class="champName">Ahri</div> 
+					<div className="col-lg-4 col-md-4 col-xs-6 thumb">
+						<a className="thumbnail">
+							<img id= "Ahri_0" className="img-responsive" src="https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Ahri_0.jpg" alt="Ahri_0"></img>
+							<div className="champName">Ahri</div> 
 						</a>
 					</div>
 				</div>
 				
-				<div class="row">
-					<div class="col-lg-4 col-md-4 col-xs-6 thumb">
-						<a class="thumbnail">
-							<img id= "Ahri_0" class="img-responsive" src="https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Ahri_0.jpg" alt="Ahri_0"></img>
-							<div class="champName">Ahri</div> 
+				<div className="row">
+					<div className="col-lg-4 col-md-4 col-xs-6 thumb">
+						<a className="thumbnail">
+							<img id= "Ahri_0" className="img-responsive" src="https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Ahri_0.jpg" alt="Ahri_0"></img>
+							<div className="champName">Ahri</div> 
 						</a>
 					</div>
-					<div class="col-lg-4 col-md-4 col-xs-6 thumb">
-						<a class="thumbnail">
-							<img id= "Ahri_0" class="img-responsive" src="https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Ahri_0.jpg" alt="Ahri_0"></img>
-							<div class="champName">Ahri</div> 
+					<div className="col-lg-4 col-md-4 col-xs-6 thumb">
+						<a className="thumbnail">
+							<img id= "Ahri_0" className="img-responsive" src="https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Ahri_0.jpg" alt="Ahri_0"></img>
+							<div className="champName">Ahri</div> 
 						</a>
 					</div>
-					<div class="col-lg-4 col-md-4 col-xs-6 thumb">
-						<a class="thumbnail">
-							<img id= "Ahri_0" class="img-responsive" src="https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Ahri_0.jpg" alt="Ahri_0"></img>
-							<div class="champName">Ahri</div> 
+					<div className="col-lg-4 col-md-4 col-xs-6 thumb">
+						<a className="thumbnail">
+							<img id= "Ahri_0" className="img-responsive" src="https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Ahri_0.jpg" alt="Ahri_0"></img>
+							<div className="champName">Ahri</div> 
 						</a>
 					</div>
 				</div>
