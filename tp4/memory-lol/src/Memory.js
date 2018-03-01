035ba01cc04344c88cd6ef52ebf161adb21a3e66
Susa Guilhem <guilhem.susa@gfi.fr>
2018-03-01 11:16:54 +0100

    {React} selectionne et place 3 champions et leurs skins _1 aleatoirement

--- tp4/memory-lol/src/Memory.js
+++ tp4/memory-lol/src/Memory.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import champs from './champions.json';
 
 class Memory extends Component {
 	/*
@@ -7,46 +8,90 @@ class Memory extends Component {
 		var state = {champs: []} ;
 	}
 	*/
-	state = {champs: [], champsSelect :[]} ;
-	
+	/*
 	getJson() {
-		fetch('/champions-info-image-stats-sort')
+		fetch('/champions')
 		.then(res => res.json())
         .then(champs => this.setState({ champs }));
 	}
-	
+	*/
 	selectChamps(){
-		//select 1
-		//put _0 _1 in champsSelect
-		//select 2
-		//check != 1
-		//put _0 _1 in champsSelect
-		//check != 1,2
-		//put _0 _1 in champsSelect
+		var listeChamps = Object.values(champs.data);
+		var nbChamp = listeChamps.length;
+		var champsSelect = []
+		
+		do{
+		var champKey1 = listeChamps[Math.trunc(Math.random() * nbChamp)]["key"];
+		var champKey2 = listeChamps[Math.trunc(Math.random() * nbChamp)]["key"];
+		var champKey3 = listeChamps[Math.trunc(Math.random() * nbChamp)]["key"];
+		}while(champKey1 === champKey2 && champKey2 === champKey3 && champKey1 === champKey3);
+		
+		champsSelect.push({
+			name:champKey1,
+			skin:0 
+		});
+		champsSelect.push({
+			name:champKey1,
+			skin:1 
+		});
+		
+		champsSelect.push({
+			name:champKey2,
+			skin:0 
+		});
+		champsSelect.push({
+			name:champKey2,
+			skin:1 
+		});
+		
+		champsSelect.push({
+			name:champKey3,
+			skin:0 
+		});
+		champsSelect.push({
+			name:champKey3,
+			skin:1 
+		});
+		
 		//shuffle
+		var j = 0;
+		var valI = '';
+		var valJ = valI;
+		var l = champsSelect.length - 1;
+		while(l > -1){
+			j = Math.floor(Math.random() * l);
+			valI = champsSelect[l];
+			valJ = champsSelect[j];
+			champsSelect[l] = valJ;
+			champsSelect[j] = valI;
+			l = l - 1;
+		}
+		return champsSelect;
 	}
 	
     render() {
-		{console.log(this.state.champs)}
+		var toPrint = this.selectChamps();
+		
 		return (
 		  <div className="Memory">
+			  
 				<div className="row">
 					<div className="col-lg-4 col-md-4 col-xs-6 thumb">
 						<a className="thumbnail">
-							<img id= "Ahri_0" className="img-responsive" src="https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Ahri_0.jpg" alt="Ahri_0"></img>
-							<div className="champName">Ahri</div> 
+							<img id= {toPrint[0].name+"_"+toPrint[0].skin} className="img-responsive" src={"https://ddragon.leagueoflegends.com/cdn/img/champion/splash/"+toPrint[0].name+"_"+toPrint[0].skin+".jpg"} alt={toPrint[0].name+"_"+toPrint[0].skin}></img>
+							<div className="champName">{toPrint[0].name}</div> 
 						</a>
 					</div>
 					<div className="col-lg-4 col-md-4 col-xs-6 thumb">
 						<a className="thumbnail">
-							<img id= "Ahri_0" className="img-responsive" src="https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Ahri_0.jpg" alt="Ahri_0"></img>
-							<div className="champName">Ahri</div> 
+							<img id= {toPrint[1].name+"_"+toPrint[1].skin} className="img-responsive" src={"https://ddragon.leagueoflegends.com/cdn/img/champion/splash/"+toPrint[1].name+"_"+toPrint[1].skin+".jpg"} alt={toPrint[1].name+"_"+toPrint[1].skin}></img>
+							<div className="champName">{toPrint[1].name}</div> 
 						</a>
 					</div>
 					<div className="col-lg-4 col-md-4 col-xs-6 thumb">
 						<a className="thumbnail">
-							<img id= "Ahri_0" className="img-responsive" src="https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Ahri_0.jpg" alt="Ahri_0"></img>
-							<div className="champName">Ahri</div> 
+							<img id= {toPrint[2].name+"_"+toPrint[2].skin} className="img-responsive" src={"https://ddragon.leagueoflegends.com/cdn/img/champion/splash/"+toPrint[2].name+"_"+toPrint[2].skin+".jpg"} alt={toPrint[2].name+"_"+toPrint[2].skin}></img>
+							<div className="champName">{toPrint[2].name}</div> 
 						</a>
 					</div>
 				</div>
@@ -54,20 +99,20 @@ class Memory extends Component {
 				<div className="row">
 					<div className="col-lg-4 col-md-4 col-xs-6 thumb">
 						<a className="thumbnail">
-							<img id= "Ahri_0" className="img-responsive" src="https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Ahri_0.jpg" alt="Ahri_0"></img>
-							<div className="champName">Ahri</div> 
+							<img id= {toPrint[3].name+"_"+toPrint[3].skin} className="img-responsive" src={"https://ddragon.leagueoflegends.com/cdn/img/champion/splash/"+toPrint[3].name+"_"+toPrint[3].skin+".jpg"} alt={toPrint[3].name+"_"+toPrint[3].skin}></img>
+							<div className="champName">{toPrint[3].name}</div> 
 						</a>
 					</div>
 					<div className="col-lg-4 col-md-4 col-xs-6 thumb">
 						<a className="thumbnail">
-							<img id= "Ahri_0" className="img-responsive" src="https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Ahri_0.jpg" alt="Ahri_0"></img>
-							<div className="champName">Ahri</div> 
+							<img id= {toPrint[4].name+"_"+toPrint[4].skin} className="img-responsive" src={"https://ddragon.leagueoflegends.com/cdn/img/champion/splash/"+toPrint[4].name+"_"+toPrint[4].skin+".jpg"} alt={toPrint[4].name+"_"+toPrint[4].skin}></img>
+							<div className="champName">{toPrint[4].name}</div> 
 						</a>
 					</div>
 					<div className="col-lg-4 col-md-4 col-xs-6 thumb">
 						<a className="thumbnail">
-							<img id= "Ahri_0" className="img-responsive" src="https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Ahri_0.jpg" alt="Ahri_0"></img>
-							<div className="champName">Ahri</div> 
+							<img id= {toPrint[5].name+"_"+toPrint[5].skin} className="img-responsive" src={"https://ddragon.leagueoflegends.com/cdn/img/champion/splash/"+toPrint[5].name+"_"+toPrint[5].skin+".jpg"} alt={toPrint[5].name+"_"+toPrint[5].skin}></img>
+							<div className="champName">{toPrint[5].name}</div> 
 						</a>
 					</div>
 				</div>
