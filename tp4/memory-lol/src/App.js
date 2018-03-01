455685b107c4e752f9d6632289cdee3e6bf8c650
Susa Guilhem <guilhem.susa@gfi.fr>
2018-03-01 15:02:57 +0100

    correction node_modules

--- tp4/memory-lol/src/App.js
+++ tp4/memory-lol/src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to MIASHS's React TP</h1>
+          <h1 className="App-title">Welcome to MIASHS's React TP : LoL Memory</h1>
         </header>
 		
 		<Memory></Memory>
