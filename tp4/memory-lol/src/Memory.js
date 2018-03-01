ee8859671052c1582095324760bd6710818ea4b1
Susa Guilhem <guilhem.susa@gfi.fr>
2018-03-01 15:46:09 +0100

    gestion du retournement des cartes

--- tp4/memory-lol/src/Memory.js
+++ tp4/memory-lol/src/Memory.js
@@ -72,6 +72,7 @@ class Memory extends Component {
 	}
 	
 	isWin(){
+		console.log("Bonnes paires : "+this.state.bonnePaires);
 		if(this.state.bonnePaires == 3){
 			alert("Vous avez gagner !");
 			window.location.reload();
@@ -80,23 +81,34 @@ class Memory extends Component {
 	checkMemory(e,idCarte){
 		var SidCarte = idCarte.toString();
 		if(this.state.clicked == ""){
+			console.log("1er carte");
 			this.state.clicked = SidCarte;
-			//document.getElementById(this.state.clicked).style.transform("rotateY(180deg)");
+			//document.getElementById(this.state.clicked).style.transform ="rotateY(180deg)";
+			//document.getElementById(this.state.clicked).style.opacity ="0";
+			document.getElementById("img"+this.state.clicked).style.opacity ="1";
+			document.getElementById("div"+this.state.clicked).style.opacity ="1";
 		}
 		else{
+			console.log("2eme carte");
 			if(SidCarte === this.state.clicked){
 				alert("Même carte sélectionée, en choisir une autre");
 			}
 			else{
 				if(SidCarte.split("_")[0] == this.state.clicked.split("_")[0]){
+					console.log("Meme champion");
 					//laisser cartes retournées
-					//document.getElementById(this.state.clicked).style.transform("rotateY(180deg)");
+					//document.getElementById(this.state.clicked).style.transform ="rotateY(180deg)";
+					document.getElementById("img"+SidCarte).style.opacity ="1";
+					document.getElementById("div"+SidCarte).style.opacity ="1";
 					this.state.bonnePaires += 1;
 				}
 				else{
+					console.log("Pas même campions");
 					//retourner cartes
-					//document.getElementById(this.state.clicked).style.transform("rotateY(180deg)");
-					//document.getElementById(this.state.clicked).style.transform("rotateY(180deg)");
+					//document.getElementById(SidCarte).style.transform ="rotateY(180deg)";
+					//document.getElementById(this.state.clicked).style.transform ="rotateY(180deg)";
+					document.getElementById("img"+this.state.clicked).style.opacity ="0";
+					document.getElementById("div"+this.state.clicked).style.opacity ="0";
 				}
 			this.state.clicked = "";
 			}
@@ -113,20 +125,20 @@ class Memory extends Component {
 				<div className="row">
 					<div className="col-lg-4 col-md-4 col-xs-6 thumb">
 						<a id={toPrint[0].name+"_"+toPrint[0].skin} className="thumbnail" onClick={ (e) => this.checkMemory(e,toPrint[0].name+"_"+toPrint[0].skin)}>
-							<img className="img-responsive" src={"https://ddragon.leagueoflegends.com/cdn/img/champion/splash/"+toPrint[0].name+"_"+toPrint[0].skin+".jpg"} alt={toPrint[0].name+"_"+toPrint[0].skin}></img>
-							<div className="champName">{toPrint[0].name}</div> 
+							<img id={"img"+toPrint[0].name+"_"+toPrint[0].skin} className="img-responsive" src={"https://ddragon.leagueoflegends.com/cdn/img/champion/splash/"+toPrint[0].name+"_"+toPrint[0].skin+".jpg"} alt={toPrint[0].name+"_"+toPrint[0].skin}></img>
+							<div id={"div"+toPrint[0].name+"_"+toPrint[0].skin} className="champName">{toPrint[0].name}</div> 
 						</a>
 					</div>
 					<div className="col-lg-4 col-md-4 col-xs-6 thumb">
 						<a id={toPrint[1].name+"_"+toPrint[1].skin}className="thumbnail" onClick={ (e) => this.checkMemory(e,toPrint[1].name+"_"+toPrint[1].skin)}>
-							<img className="img-responsive" src={"https://ddragon.leagueoflegends.com/cdn/img/champion/splash/"+toPrint[1].name+"_"+toPrint[1].skin+".jpg"} alt={toPrint[1].name+"_"+toPrint[1].skin}></img>
-							<div className="champName">{toPrint[1].name}</div> 
+							<img id={"img"+toPrint[1].name+"_"+toPrint[1].skin} className="img-responsive" src={"https://ddragon.leagueoflegends.com/cdn/img/champion/splash/"+toPrint[1].name+"_"+toPrint[1].skin+".jpg"} alt={toPrint[1].name+"_"+toPrint[1].skin}></img>
+							<div id={"div"+toPrint[1].name+"_"+toPrint[1].skin} className="champName">{toPrint[1].name}</div> 
 						</a>
 					</div>
 					<div className="col-lg-4 col-md-4 col-xs-6 thumb">
 						<a id= {toPrint[2].name+"_"+toPrint[2].skin} className="thumbnail" onClick={ (e) => this.checkMemory(e,toPrint[2].name+"_"+toPrint[2].skin)}>
-							<img className="img-responsive" src={"https://ddragon.leagueoflegends.com/cdn/img/champion/splash/"+toPrint[2].name+"_"+toPrint[2].skin+".jpg"} alt={toPrint[2].name+"_"+toPrint[2].skin}></img>
-							<div className="champName">{toPrint[2].name}</div> 
+							<img id={"img"+toPrint[2].name+"_"+toPrint[2].skin}className="img-responsive" src={"https://ddragon.leagueoflegends.com/cdn/img/champion/splash/"+toPrint[2].name+"_"+toPrint[2].skin+".jpg"} alt={toPrint[2].name+"_"+toPrint[2].skin}></img>
+							<div id={"div"+toPrint[2].name+"_"+toPrint[2].skin} className="champName">{toPrint[2].name}</div> 
 						</a>
 					</div>
 				</div>
@@ -134,20 +146,20 @@ class Memory extends Component {
 				<div className="row">
 					<div className="col-lg-4 col-md-4 col-xs-6 thumb">
 						<a id= {toPrint[3].name+"_"+toPrint[3].skin} className="thumbnail" onClick={ (e) => this.checkMemory(e,toPrint[3].name+"_"+toPrint[3].skin)}>
-							<img className="img-responsive" src={"https://ddragon.leagueoflegends.com/cdn/img/champion/splash/"+toPrint[3].name+"_"+toPrint[3].skin+".jpg"} alt={toPrint[3].name+"_"+toPrint[3].skin}></img>
-							<div className="champName">{toPrint[3].name}</div> 
+							<img id={"img"+toPrint[3].name+"_"+toPrint[3].skin} className="img-responsive" src={"https://ddragon.leagueoflegends.com/cdn/img/champion/splash/"+toPrint[3].name+"_"+toPrint[3].skin+".jpg"} alt={toPrint[3].name+"_"+toPrint[3].skin}></img>
+							<div id={"div"+toPrint[3].name+"_"+toPrint[3].skin} className="champName">{toPrint[3].name}</div> 
 						</a>
 					</div>
 					<div className="col-lg-4 col-md-4 col-xs-6 thumb">
 						<a id= {toPrint[4].name+"_"+toPrint[4].skin} className="thumbnail" onClick={ (e) => this.checkMemory(e,toPrint[4].name+"_"+toPrint[4].skin)}>
-							<img className="img-responsive" src={"https://ddragon.leagueoflegends.com/cdn/img/champion/splash/"+toPrint[4].name+"_"+toPrint[4].skin+".jpg"} alt={toPrint[4].name+"_"+toPrint[4].skin}></img>
-							<div className="champName">{toPrint[4].name}</div> 
+							<img id={"img"+toPrint[4].name+"_"+toPrint[4].skin}className="img-responsive" src={"https://ddragon.leagueoflegends.com/cdn/img/champion/splash/"+toPrint[4].name+"_"+toPrint[4].skin+".jpg"} alt={toPrint[4].name+"_"+toPrint[4].skin}></img>
+							<div id={"div"+toPrint[4].name+"_"+toPrint[4].skin} className="champName">{toPrint[4].name}</div> 
 						</a>
 					</div>
 					<div className="col-lg-4 col-md-4 col-xs-6 thumb">
 						<a id= {toPrint[5].name+"_"+toPrint[5].skin} className="thumbnail" onClick={ (e) => this.checkMemory(e,toPrint[5].name+"_"+toPrint[5].skin)}>
-							<img className="img-responsive" src={"https://ddragon.leagueoflegends.com/cdn/img/champion/splash/"+toPrint[5].name+"_"+toPrint[5].skin+".jpg"} alt={toPrint[5].name+"_"+toPrint[5].skin}></img>
-							<div className="champName">{toPrint[5].name}</div> 
+							<img id={"img"+toPrint[5].name+"_"+toPrint[5].skin} className="img-responsive" src={"https://ddragon.leagueoflegends.com/cdn/img/champion/splash/"+toPrint[5].name+"_"+toPrint[5].skin+".jpg"} alt={toPrint[5].name+"_"+toPrint[5].skin}></img>
+							<div id={"div"+toPrint[5].name+"_"+toPrint[5].skin} className="champName">{toPrint[5].name}</div> 
 						</a>
 					</div>
 				</div>
